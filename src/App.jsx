8711fcd64528a754c910cf8cc37ca6407d733c1c
Jia Earn <jiae4rn@gmail.com>
2024-02-03 14:17:24 +0800

    Initial commit

---
+++ src/App.jsx
@@ -0,0 +1,36 @@
+
+export default function App() {
+  return (
+    <div>
+
+      <div className="flex bg-blue gap-2 mt-10 mx-10">
+
+        <div className="flex-1 bg-grey1 font-fira-code h-[50px]">
+          <p className="text-[#AB5D4A]">Test1</p>
+        </div>
+        <div className="flex-1 bg-grey2 font-fira-code h-[50px]">
+          <p className="text-[#008B8B]">Test2</p>
+        </div>
+
+        <div className="flex-1 bg-grey3 font-fira-code h-[50px]">
+          <p className="text-[#AB5D4A]">Test3</p>
+        </div>
+
+        <div className="flex-1 bg-grey4 font-fira-code h-[50px]">
+          <p className="text-[#AB5D4A]">Test5</p>
+        </div>
+
+
+        <div className="flex-1 bg-grey5 font-fira-code h-[50px]">
+          <p className="text-[#AB5D4A]">Test6</p>
+        </div>
+
+      </div>
+
+    </div>
+
+
+    
+
+  )
+}
---
+++ tailwind.config.js
@@ -0,0 +1,61 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+
+      colors: {
+
+        black1: "#00040f",
+        textwhite: "rgba(255, 255, 255, 0.7)",
+        textblue: "rgba(9, 151, 124, 0.1)",
+
+        brightgreen: '#7FFFD4',
+        brightblue: '#00f6ff',
+
+        coral1: '#FFC3B7',
+        coral2: '#FFAA9C',
+        coral3: '#E0705B',
+        coral4: '#FF7F50',
+
+        yellow1: '#FFD700',
+        yellow2: '#FFD68C',
+        yellow3: '#FFEEAD',
+        
+        blue1: '#0058CC',
+        blue2: '#0077FF',
+        blue3: '#3399FF',
+        blue4: '#66BBFF',
+        blue5: '#99DDFF',
+        blue6: '#CCEEFF',
+
+        coral: '#AEEEEE',
+        coral2: '#40E0D0',
+        coral3: '#00CED1',
+        
+        grey1: '#F5F5F5',
+        grey2: '#E0E0E0',
+        grey3: '#CCCCCC',
+        grey4: '#999999',
+        grey5: '#666666',
+      },
+      fontFamily: {
+        'roboto': ['Roboto', 'sans-serif'],
+        'fira-code': ['Fira Code', 'monospace']
+      },
+
+    },
+    screens: {
+      xs: "480px",
+      ss: "620px",
+      sm: "768px",
+      md: "1060px",
+      lg: "1200px",
+      xl: "1700px",
+    },
+  },
+  plugins: [],
+}
