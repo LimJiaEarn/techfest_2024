8f2a232ab028eef6ab2c9e026a037a26de78f250
Jia Earn <jiae4rn@gmail.com>
2024-02-05 14:02:30 +0800

    Completed Styling for Experience and UploadResume

--- Frontend/src/components/MyExperience.jsx
+++ Frontend/src/components/MyExperience.jsx
@@ -58,71 +58,83 @@ function MyExperience() {
     };
 
     return (
-        <div className="bg-gray-800 text-white rounded-xl shadow-md p-6">
+        <div className="bg-gray-800 text-white rounded-md-xl shadow-md p-6">
 
             <div className="mb-4">
                 <h2 className="text-2xl font-bold mb-2">My Experience</h2>
                 <div className="border-b border-gray-600"></div>
             </div>
             
-            <div className="mb-4">
+            <div className="mb-4 mt-6">
                 <h3 className="text-lg font-semibold mb-2">Email</h3>
                 {isEditingEmail ? (
                     <div className="flex items-center">
-                        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="border border-gray-400 px-3 py-1 rounded mr-2 bg-gray-700 text-white focus:outline-none w-full" />
-                        <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600" onClick={handleSaveEmail}>Save</button>
+                        <input type="text" value={email} onChange={(e) => setEmail(e.target.value)} className="border border-gray-400 px-3 py-1 rounded-md mr-2 bg-gray-700 text-white focus:outline-none w-full" />
+                        <button className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600" onClick={handleSaveEmail}>Save</button>
                     </div>
                 ) : (
-                    <>
+                    <div className="mt-2">
                         <p className="text-gray-300">{email}</p>
-                        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 ml-2" onClick={handleEmailEdit}>Edit</button>
-                        <button className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 ml-2" onClick={clearEmail}>Clear Email</button>
-                    </>
+                        <div className="mt-2">
+                        <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 ml-2" onClick={handleEmailEdit}>Edit</button>
+                        <button className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 ml-2" onClick={clearEmail}>Clear Email</button>
+                        </div>
+                    </div>
+
                 )}
             </div>
-            <div className="mb-4">
+            <div className="mb-4 mt-6">
                 <h3 className="text-lg font-semibold mb-2">Skills</h3>
                 {isEditingSkills ? (
                     <div className="flex items-center">
-                        <input type="text" value={skills} onChange={(e) => setSkills(e.target.value)} className="border border-gray-400 px-3 py-1 rounded mr-2 bg-gray-700 text-white focus:outline-none w-full" />
-                        <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600" onClick={handleSaveSkills}>Save</button>
+                        <input type="text" value={skills} onChange={(e) => setSkills(e.target.value)} className="border border-gray-400 px-3 py-1 rounded-md mr-2 bg-gray-700 text-white focus:outline-none w-full" />
+                        <button className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600" onClick={handleSaveSkills}>Save</button>
                     </div>
                 ) : (
-                    <>
+                    <div className="mt-2">
                         <p className="text-gray-300">{skills}</p>
-                        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 ml-2" onClick={handleSkillsEdit}>Edit</button>
-                        <button className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 ml-2" onClick={clearSkills}>Clear Skills</button>
-                    </>
+                        <div className="mt-2">
+                        <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 ml-2" onClick={handleSkillsEdit}>Edit</button>
+                        <button className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 ml-2" onClick={clearSkills}>Clear Skills</button>
+                        </div>
+                    </div>
+
                 )}
             </div>
-            <div className="mb-4">
+            <div className="mb-4 mt-6">
                 <h3 className="text-lg font-semibold mb-2">Education</h3>
                 {isEditingEducation ? (
                     <div className="flex items-center">
-                        <input type="text" value={education} onChange={(e) => setEducation(e.target.value)} className="border border-gray-400 px-3 py-1 rounded mr-2 bg-gray-700 text-white focus:outline-none w-full" />
-                        <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600" onClick={handleSaveEducation}>Save</button>
+                        <input type="text" value={education} onChange={(e) => setEducation(e.target.value)} className="border border-gray-400 px-3 py-1 rounded-md mr-2 bg-gray-700 text-white focus:outline-none w-full" />
+                        <button className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600" onClick={handleSaveEducation}>Save</button>
                     </div>
                 ) : (
-                    <>
+                    <div className="mt-2">
                         <p className="text-gray-300">{education}</p>
-                        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 ml-2" onClick={handleEducationEdit}>Edit</button>
-                        <button className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 ml-2" onClick={clearEducation}>Clear Education</button>
-                    </>
+                        <div className="mt-2">
+                        <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 ml-2" onClick={handleEducationEdit}>Edit</button>
+                        <button className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 ml-2" onClick={clearEducation}>Clear Education</button>
+                        </div>
+                    </div>
+
                 )}
             </div>
-            <div className="mb-4">
+            <div className="mb-4 mt-6">
                 <h3 className="text-lg font-semibold mb-2">Work Experience</h3>
                 {isEditingWorkExperience ? (
                     <div className="flex items-center">
-                        <input type="text" value={workExperience} onChange={(e) => setWorkExperience(e.target.value)} className="border border-gray-400 px-3 py-1 rounded mr-2 bg-gray-700 text-white focus:outline-none w-full" />
-                        <button className="px-4 py-2 bg-green-500 text-white rounded hover:bg-green-600" onClick={handleSaveWorkExperience}>Save</button>
+                        <input type="text" value={workExperience} onChange={(e) => setWorkExperience(e.target.value)} className="border border-gray-400 px-3 py-1 rounded-md mr-2 bg-gray-700 text-white focus:outline-none w-full" />
+                        <button className="px-4 py-2 bg-green-500 text-white rounded-md hover:bg-green-600" onClick={handleSaveWorkExperience}>Save</button>
                     </div>
                 ) : (
-                    <>
+                    <div className="mt-2">
                         <p className="text-gray-300">{workExperience}</p>
-                        <button className="px-4 py-2 bg-blue-500 text-white rounded hover:bg-blue-600 ml-2" onClick={handleWorkExperienceEdit}>Edit</button>
-                        <button className="px-4 py-2 bg-red-500 text-white rounded hover:bg-red-600 ml-2" onClick={clearWorkExperience}>Clear Work Experience</button>
-                    </>
+                        <div className="mt-2">
+                        <button className="px-4 py-2 bg-blue-500 text-white rounded-md hover:bg-blue-600 ml-2" onClick={handleWorkExperienceEdit}>Edit</button>
+                        <button className="px-4 py-2 bg-red-500 text-white rounded-md hover:bg-red-600 ml-2" onClick={clearWorkExperience}>Clear Work Experience</button>
+                        </div>
+                    </div>
+
                 )}
             </div>
         </div>
