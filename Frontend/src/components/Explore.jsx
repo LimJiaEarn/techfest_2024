5fae95e0e61329a76b2ec51ee59c53d6d90b78d9
Jia Earn <jiae4rn@gmail.com>
2024-02-06 09:34:12 +0800

    Improved UX for Explore.jsx

--- Frontend/src/components/Explore.jsx
+++ Frontend/src/components/Explore.jsx
@@ -10,6 +10,7 @@ const Explore = () => {
   
   const [jobListings, setJobListings] = useState([]);
   const [AIMessage, setAIMessage] = useState("");
+  const [typedMessage, setTypedMessage] = useState("");
   const jobListRef = useRef(null);
 
   useEffect(() => {
@@ -22,8 +23,7 @@ const Explore = () => {
   const fetchAIMsg = async () => {
     // hardcoded in
     try {
-        const hardcodedMessage = "You should study harder";
-          
+        const hardcodedMessage = "FFantastic job on acquiring in-demand skills like REST API and C#! It's evident that you're actively investing in your professional growth. As you continue your journey, consider allocating some time to enhance your proficiency in SQL and Python, two highly sought-after skills in today's job market. With your determination and commitment to improvement, mastering these technologies will open up even more opportunities and broaden your career prospects. Keep up the great work, and remember, every step you take towards skill enhancement brings you closer to your career aspirations! You've got this!";
           
         setAIMessage(hardcodedMessage);
     } catch (error) {
@@ -35,58 +35,74 @@ const Explore = () => {
   const fetchJobListings = async () => {
     // hardcoded in
     try {
-        const jobListingsData = [
-            {
-              id: 1,
-              company: "TikTok",
-              title: "Software Engineer (Cloud Infrastructure)",
-              salary: '-',
-              description: "Looking for a Software Engineer to join the messaging middleware team, responsible for developing and advancing a messaging platform.",
-              requiredSkills: [
-                "C","C++","Go","Python",
-                "Operating Systems",
-                "Kafka","NSQ","RocketMQ","Pulsar"]
-            },
-            {
-              id: 2,
-              company: 'Software Engineer (Backend)',
-              title: 'Shopback, Singapore',
-              salary: '-',
-              description: 'Contribute to the ideation, technical design, implementation, and testing of product features.',
-              requiredSkills: ['REST API', 'Technical Design', 'Product Testing'],
-            },
-            {
-              id: 3,
-              company: 'UBS, Singapore',
-              title: 'C# Software Engineer',
-              salary: '-',
-              description: 'Design, develop, and improve the digital products and technology services we provide to our clients and employees',
-              requiredSkills: ['SQL', 'C#', '.NET', 'Agile'],
-            },
-            {
-              id: 4,
-              company: 'GIC, Singapore',
-              title: 'Software Developer',
-              salary: '-',
-              description: 'Perform hands-on analysis of the technical, data and business functions and devise implementation plans',
-              requiredSkills: ['Unix', 'SQL', 'Docker', 'Python', 'Javascript', 'ReactJS'],
-            },
-            {
-              id: 5,
-              company: 'Illumina, Singapore',
-              title: 'Software Engineer',
-              salary: '-',
-              description: 'Responsible for designing, developing, testing, and maintaining software applications',
-              requiredSkills: ['Java', 'C#', 'Angular', 'AWS-RDS', 'SQL', 'REST API'],
-            },
-          ];
-          
+      const jobListingsData = [
+        {
+          id: 1,
+          company: "TikTok",
+          title: "Software Engineer (Cloud Infrastructure)",
+          salary: '-',
+          description: "Looking for a Software Engineer to join the messaging middleware team, responsible for developing and advancing a messaging platform.",
+          requiredSkills: [
+            "C","C++","Go","Python",
+            "Operating Systems",
+            "Kafka","NSQ","RocketMQ","Pulsar"]
+        },
+        {
+          id: 2,
+          company: 'Software Engineer (Backend)',
+          title: 'Shopback, Singapore',
+          salary: '-',
+          description: 'Contribute to the ideation, technical design, implementation, and testing of product features.',
+          requiredSkills: ['REST API', 'Technical Design', 'Product Testing'],
+        },
+        {
+          id: 3,
+          company: 'UBS, Singapore',
+          title: 'C# Software Engineer',
+          salary: '-',
+          description: 'Design, develop, and improve the digital products and technology services we provide to our clients and employees',
+          requiredSkills: ['SQL', 'C#', '.NET', 'Agile'],
+        },
+        {
+          id: 4,
+          company: 'GIC, Singapore',
+          title: 'Software Developer',
+          salary: '-',
+          description: 'Perform hands-on analysis of the technical, data and business functions and devise implementation plans',
+          requiredSkills: ['Unix', 'SQL', 'Docker', 'Python', 'Javascript', 'ReactJS'],
+        },
+        {
+          id: 5,
+          company: 'Illumina, Singapore',
+          title: 'Software Engineer',
+          salary: '-',
+          description: 'Responsible for designing, developing, testing, and maintaining software applications',
+          requiredSkills: ['Java', 'C#', 'Angular', 'AWS-RDS', 'SQL', 'REST API'],
+        },
+      ];
           
       setJobListings(jobListingsData);
     } catch (error) {
       console.error('Error fetching job listings:', error);
     }
   };
+
+  const typeMessage = (message) => {
+    let index = 0;
+    const typingInterval = setInterval(() => {
+      setTypedMessage((prevMessage) => prevMessage + message.charAt(index));
+      index++;
+      if (index === message.length) {
+        clearInterval(typingInterval);
+      }
+    }, 200); 
+  };
+
+  useEffect(() => {
+    if (AIMessage) {
+      typeMessage(AIMessage);
+    }
+  }, [AIMessage]);
 
   if (jobPreferences.length === 0) {
     return (
@@ -127,6 +143,16 @@ const Explore = () => {
     });
   });
 
+  // Convert matched skill counts to an array of objects for sorting
+  const matchedSkillsSorted = Object.entries(matchedSkillCounts)
+    .map(([skill, count]) => ({ skill, count }))
+    .sort((a, b) => b.count - a.count);
+
+  // Convert missing skill counts to an array of objects for sorting
+  const missingSkillsSorted = Object.entries(missingSkillCounts)
+    .map(([skill, count]) => ({ skill, count }))
+    .sort((a, b) => b.count - a.count);
+  
   const scrollToNextListing = (direction) => {
     if (jobListRef.current) {
       const scrollAmount = direction === 'up' ? -300 : 300;
@@ -144,9 +170,9 @@ const Explore = () => {
             <div className="flex justify-between items-center ">
                 <div className="bg-gray-800 p-4 rounded ">
                     <div className="mb-4">
-                        <h3 className="text-lg font-semibold text-gray-400">Matched Skills:</h3>
+                        <h3 className="text-24px text-center font-semibold text-gray-400">Matched Skills</h3>
                         <div className="flex flex-wrap gap-2">
-                            {Object.entries(matchedSkillCounts).map(([skill, count]) => (
+                            {matchedSkillsSorted.map(({ skill, count }) => (
                             <div key={skill} className="bg-green-600 text-gray-200 py-1 px-2 rounded">
                                 {skill} (x{count})
                             </div>
@@ -154,9 +180,9 @@ const Explore = () => {
                         </div>
                     </div>
                     <div>
-                        <h3 className="text-lg font-semibold text-gray-400">Missing Skills:</h3>
+                        <h3 className="text-24px text-center font-semibold text-gray-400">Missing Skills</h3>
                         <div className="flex flex-wrap gap-2">
-                            {Object.entries(missingSkillCounts).map(([skill, count]) => (
+                            {missingSkillsSorted.map(({ skill, count }) => (
                             <div key={skill} className="bg-red-600 text-gray-200 py-1 px-2 rounded">
                                 {skill} (x{count})
                             </div>
@@ -171,8 +197,8 @@ const Explore = () => {
             <h1 className="text-32px font-bold text-gradient text-center mb-4">
                 Our AI Recommendation
             </h1>
-            <div className="bg-gray-800 text-white p-4 rounded">
-                <p>{AIMessage}</p>
+            <div className="bg-gray-800 font-roboto text-gray-400 text-20px p-4 rounded">
+                <p>{typedMessage}</p>
             </div>
         </div>
 
